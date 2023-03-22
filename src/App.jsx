c655e402c7ec001be56df82f9a52172db9b59180
joshie ngumbau <ngumbaujoshua4@gmail.com>
2023-03-22 17:56:39 +0300

    chatbot

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
-const API_KEY = "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";
+const API_KEY = "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";
 // "Explain things like you would to a 10 year old learning how to code."
 const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
   "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
